ea87d282ca98e511d09007d664d70e95d138e20c
Jeremy Ling <jeremy.ling@navagis.com>
2017-06-26 02:00:36 +0800

    duty_reports html

---
+++ application/views/duty_reports.php
@@ -0,0 +1,84 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="<?= base_url();?>bootstrap/css/bootstrap.css" rel="stylesheet">
+    <script src="<?= base_url();?>bootstrap/js/jquery-3.2.1.min.js"></script>
+    <script src="<?= base_url();?>bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <?php include 'base.inc'; ?>
+    <div>&nbsp;</div>
+    <div class="col-lg-11">
+        <fieldset>
+            <legend>Duty Reports Options</legend>
+            <form class="form-horizontal" role="form" action='<?= base_url();?>index.php/main/duty_reports' method="post">
+                <div class="form-group">
+                    <div class="bs-example" data-example-id="single-button-dropdown">
+                        <label for="title" class="control-label">From:*</label>
+                        <div class="btn-group">
+                            <input type="date" class="form-control" name="select_from" id="select_from" value="<?php echo $selected_from;?>" >
+                        </div>
+                        <label for="title" class="control-label">To:*</label>
+                        <div class="btn-group">
+                            <input type="date" class="form-control" name="select_to" id="select_to" value="<?php echo $selected_to;?>">
+                        </div>
+                        <label for="title" class="control-label">Sort By:*</label>
+                        <div class="btn-group">
+                            <select class="form-control" name="sort_by" id="sort_by">
+                                <option value="0" <?php if ($sort_by == 0) echo 'selected';?>>Unit</option>
+                                <option value="1" <?php if ($sort_by == 1) echo 'selected';?>>Highest Rendered</option>
+                                <option value="2" <?php if ($sort_by == 2) echo 'selected';?>>Highest Lates on Add</option>
+                                <option value="3" <?php if ($sort_by == 3) echo 'selected';?>>Highest Lates on Duty</option>
+                                <option value="4" <?php if ($sort_by == 4) echo 'selected';?>>Lowest Rendered</option>
+                                <option value="5" <?php if ($sort_by == 5) echo 'selected';?>>Lowest Lates on Add</option>
+                                <option value="6" <?php if ($sort_by == 6) echo 'selected';?>>Lowest Lates on Duty</option>
+                            </select>
+                        </div>
+                        <div class="btn-group">
+                            <button type="submit" class="btn btn-success" >Search</button>
+                        </div>
+                        <div class="btn-group">
+                            <a href="<?= base_url();?>index.php/main/duty_reports" class="btn btn-default" >Clear</a>
+                        </div>
+                        <div class="btn-group">
+                            <a href="<?= base_url();?>index.php/main" class="btn btn-info" >Home</a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div>&nbsp;</div>
+            Shown From <span style="color:red;font-size:16px;"><?=date('M d, Y',strtotime($selected_from)); ;?></span> -- <span style="color:red;font-size:16px;"><?=date('M d, Y',strtotime($selected_to)); ;?></span>
+            <table class="table table-striped table-bordered table-hover" id="dataTables-example"  style="font-size:14px;">
+                <thead>
+                <tr>
+                    <th>UNIT #</th>
+                    <th>Total Duty</th>
+                    <th>Lates on Duty</th>
+                    <th>Total Add</th>
+                    <th>Lates on Add</th>
+                    <th>Total Rendered</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php if (isset($duties)) {
+                    foreach ($duties as $key => $n) {?>
+                        <tr>
+                            <td><?php echo $n->unit; ?></td>
+                            <td><?php echo $n->total_duty; ?></td>
+                            <td><?php echo $n->total_duty_late; ?></td>
+                            <td><?php echo $n->total_add; ?></td>
+                            <td><?php echo $n->total_add_late; ?></td>
+                            <td><?php echo $n->total_rendered; ?></td>
+                        </tr>
+                    <?php } ?>
+                <?php }?>
+                </tbody>
+            </table>
+        </fieldset>
+    </div>
+</div>
+</body>
+</html>
